7e7c33e8a6761fe9590afb6c033550c50ad214c5
Ewelina Ślepko <slepko.ewelina@gmail.com>
2021-09-15 19:51:55 +0200

    Add Angular project

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FormComponent} from './form/form.component';
+import {ResizableElementComponent} from './shared/resizable-element/resizable-element.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormComponent,
+    ResizableElementComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
